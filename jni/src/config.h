639b81c8deb9e6ebd558759f88e38324f2deb72a
Hikari Genzo <leonxp@libero.it>
2019-08-19 13:29:27 +0200

    Test C Completed!

---
+++ jni/src/config.h
@@ -0,0 +1,7 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define APPNAME "HelloNDK"
+//#define LOGFILEPATH "/data/local/tmp/HelloNDK/log/"
+
+#endif
---
+++ jni/src/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "custom/log.h"
+#include "custom/util.h"
+
+#if defined _WIN64 || defined _WIN32
+    #include <Windows.h>
+#else
+    #include <unistd.h>
+#endif
+
+int main(int argc, char* argv[]){
+#if defined _WIN64 || defined _WIN32
+    // disable buffering, we want logs immediately
+    // even line buffering (setvbuf() with mode _IOLBF) is not sufficient
+    setbuf(stdout, NULL);
+    setbuf(stderr, NULL);
+#endif
+    struct timeval st, et;
+    getTick(&st);
+
+	infoLog("Application starting ...");
+
+    // Fake Sleep
+    #if defined _WIN64 || defined _WIN32
+        Sleep(10);
+    #else
+        usleep(10);
+    #endif
+
+    end:
+    getTick(&et);
+    infoLog("Application Completed. ETA %s", getETA(st, et));
+
+    return 0;
+}
